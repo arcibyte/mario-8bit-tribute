430d90a3b1097dd0e9641698360a6a96f9bcfe6e
arcibyte <arcibyte@gmail.com>
2025-07-09 22:40:46 -0500

    ✨ feat: configure Tailwind CSS and add Header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://tu-usuario.github.io",
   base: "/mario-8bit-tribute",
   output: "static",
-
   build: {
     assets: "assets",
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
   },
 });
